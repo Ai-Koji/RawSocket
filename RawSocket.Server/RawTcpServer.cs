a61b301f35fe323eccdd6e89829713e46ac06789
Lost-S0ul <kireev.ivan.8@gmail.com>
2025-05-11 23:34:21 +0700

    feat: handlepacket

--- RawSocket.Server/RawTcpServer.cs
+++ RawSocket.Server/RawTcpServer.cs
@@ -35,6 +35,15 @@ namespace SimpleTcpSniffer
         private ConcurrentDictionary<IPEndPoint, IRawTcpConnection> _activeConnections = new();
         private readonly ConcurrentDictionary<IPEndPoint, HandshakeState> _handshakeStates = new();
 
+        private void SendPacket(byte[] bytes) => _device.SendPacket(bytes);
+        public IRawTcpConnection AcceptRawTcpConnection()
+        {
+            return _acceptedConnections.Take();
+        }
+        public async Task<IRawTcpConnection> AcceptRawTcpConnectionAsync(CancellationToken cancellationToken = default)
+        {
+            return await Task.Run(() => _acceptedConnections.Take(cancellationToken), cancellationToken);
+        }
         public RawTcpSniffer(IPEndPoint localEndPoint)
         {
             if (!_isCapturing) // запущен ли сниффер
@@ -78,34 +87,93 @@ namespace SimpleTcpSniffer
             Stop();
             _device.OnPacketArrival -= HandlePacket;
         }
-        // обрабатывает каждый пакет
         private void HandlePacket(object sender, PacketCapture packetCapture)
         {
             var rawPacket = packetCapture.GetPacket();
-            var packet = rawPacket.GetPacket();
+            var packet = Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
             var ipPacket = packet.Extract<IPv4Packet>();
             var tcpPacket = packet.Extract<TcpPacket>();
 
-            Console.WriteLine($"{rawPacket} {packet} {ipPacket} {tcpPacket}");
+            if (ipPacket == null || tcpPacket == null)
+                return;
 
-            if (ipPacket != null && tcpPacket != null)
+            var remoteEndPoint = new IPEndPoint(ipPacket.SourceAddress, tcpPacket.SourcePort);
+            var localEndPoint = new IPEndPoint(ipPacket.DestinationAddress, tcpPacket.DestinationPort);
+
+            if (!localEndPoint.Address.Equals(_endPoint.Address))
+                return;
+
+            if (tcpPacket.Synchronize && !tcpPacket.Acknowledgment)
             {
-                // Извлечение необходимых данных
-                var ipSource = ipPacket.SourceAddress.ToString();
-                var portSource = tcpPacket.SourcePort;
-                var ipDest = ipPacket.DestinationAddress.ToString();
-                var portDest = tcpPacket.DestinationPort;
-                var sequenceNumber = tcpPacket.SequenceNumber;
-                var flags = tcpPacket.Flags;
+                HandleSynPacket(remoteEndPoint, tcpPacket);
+            }
+            else if (tcpPacket.Acknowledgment && !tcpPacket.Synchronize)
+            {
+                HandleFinalAck(remoteEndPoint, tcpPacket);
+            }
+            else if (tcpPacket.PayloadData != null && tcpPacket.PayloadData.Length > 0)
+            {
+                HandlePayloadPacket(remoteEndPoint, tcpPacket);
+            }
+        }
 
-                // Вывод данных
-                Console.WriteLine($"IP Source: {ipSource}");
-                Console.WriteLine($"Port Source: {portSource}");
-                Console.WriteLine($"IP Destination: {ipDest}");
-                Console.WriteLine($"Port Destination: {portDest}");
-                Console.WriteLine($"Sequence Number: {sequenceNumber}");
-                Console.WriteLine($"Flags: {flags}");
-                Console.WriteLine();
+        private void HandleSynPacket(IPEndPoint remoteEndPoint, TcpPacket tcpPacket)
+        {
+            var serverSeq = (uint)new Random().Next(0, int.MaxValue);
+
+            _handshakeStates[remoteEndPoint] = new HandshakeState
+            {
+                ClientSeq = tcpPacket.SequenceNumber,
+                ServerSeq = serverSeq,
+                Timestamp = DateTime.Now
+            };
+
+            var synAckPacket = new TcpHeaderBuilder(_device.Interface.MacAddress)
+                .From(_endPoint.Address, _endPoint.Port)
+                .To(remoteEndPoint.Address, remoteEndPoint.Port)
+                .WithSynAck()
+                .WithSequence(serverSeq)
+                .WithAck(tcpPacket.SequenceNumber + 1)
+                .Build();
+
+            SendPacket(synAckPacket.Bytes);
+        }
+
+        private void HandleFinalAck(IPEndPoint remoteEndPoint, TcpPacket tcpPacket)
+        {
+            if (!_handshakeStates.TryGetValue(remoteEndPoint, out var handshakeState))
+                return;
+
+            if (tcpPacket.AcknowledgmentNumber != handshakeState.ServerSeq + 1)
+                return;
+
+            var connection = new RawTcpConnection(
+                localEndPoint: new IPEndPoint(_endPoint.Address, _endPoint.Port),
+                remoteEndPoint: remoteEndPoint,
+                sendAction: SendPacket);
+
+            _activeConnections.TryAdd(remoteEndPoint, connection);
+
+            _acceptedConnections.Add(connection);
+
+            _handshakeStates.TryRemove(remoteEndPoint, out _);
+        }
+
+        private void HandlePayloadPacket(IPEndPoint remoteEndPoint, TcpPacket tcpPacket)
+        {
+            if (_activeConnections.TryGetValue(remoteEndPoint, out var connection))
+            {
+                connection.EnqueueReceivedData(tcpPacket.PayloadData);
+
+                var ackPacket = new TcpHeaderBuilder(_device.Interface.MacAddress)
+                    .From(_endPoint.Address, _endPoint.Port)
+                    .To(remoteEndPoint.Address, remoteEndPoint.Port)
+                    .WithAckOnly()
+                    .WithSequence(tcpPacket.AcknowledgmentNumber)
+                    .WithAck(tcpPacket.SequenceNumber + (uint)tcpPacket.PayloadData.Length)
+                    .Build();
+
+                SendPacket(ackPacket.Bytes);
             }
         }
     }
