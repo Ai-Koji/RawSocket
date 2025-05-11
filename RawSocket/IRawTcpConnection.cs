224e3c70d556842fee2584a5bc40e71cf38ad390
Lost-S0ul <kireev.ivan.8@gmail.com>
2025-05-11 22:32:10 +0700

    feat: added few interfaces for tcp connect and server

---
+++ RawSocket/IRawTcpConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawSocket.Common
+{
+    public interface IRawTcpConnection : IDisposable
+    {
+        IPEndPoint LocalEndPoint { get; }
+        IPEndPoint RemoteEndPoint { get; }
+        RawTcpState State { get; }
+        void Send(byte[] data);
+        byte[] Receive();
+        Task<byte[]> ReceiveAsync(CancellationToken cancellationToken = default);
+        void Close();
+        void EnqueueReceivedData(byte[] data);
+    }
+}
