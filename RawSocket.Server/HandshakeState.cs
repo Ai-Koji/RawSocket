90104af6d87ed1e3376402bc21883cf03927ede1
Lost-S0ul <kireev.ivan.8@gmail.com>
2025-05-11 22:57:44 +0700

    feat: handshake

---
+++ RawSocket.Server/HandshakeState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawSocket.Server
+{
+    public class HandshakeState
+    {
+        public uint ServerSeq { get; set; }
+        public uint ClientSeq { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+
+}
